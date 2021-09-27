b4677b64105988f6100a2ece336c713e981cfbdd
leandrosimonato <leandrosimonato@gmail.com>
2021-09-27 16:52:29 -0300

    Criando banco de dados e configurando conexão com o TYPEORM.

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,15 +1,15 @@
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use(routes);
-
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
@@ -20,11 +20,10 @@ app.use(
     }
     return response.status(500).json({
       status: 'error',
-      message: 'Internal Server Error',
+      message: 'Internal server error',
     });
   },
 );
-
 app.listen(3333, () => {
-  console.log('Server started on port 3333!');
+  console.log('Server started on port 3333! 🏆');
 });

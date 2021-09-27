f0ca41b309317c3a23cc81f6b35e6207c85f54e3
leandrosimonato <leandrosimonato@gmail.com>
2021-09-27 15:34:21 -0300

    Install Programm.

---
+++ src/shared/http/server.ts
@@ -0,0 +1,30 @@
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  },
+);
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333!');
+});
